91435bf1ff3537127abfb72d0446b94625c838e2
endrius <endrius.pontes@escola.pr.gov.br>
2025-06-27 11:33:20 -0300

    capitulo3

--- capitulo3/lista/exercicio1.py
+++
@@ -1,8 +0,0 @@
-def justificar_direita(stringQualquer):
-    setentaEspaços = "                    "
-    print(len(setentaEspaços))
-    concatena = setentaEspaços + stringQualquer
-    print(setentaEspaços * 7 + stringQualquer)
-
-stringQualquer = "Rafael"
-justificar_direita(stringQualquer)
