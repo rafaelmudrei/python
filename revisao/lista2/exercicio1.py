082c5ec3f4eca3cb8363d95bafd4e3efeec00e6b
mateus <mateus.gomes.dias@escola.pr.gov.br>
2025-10-02 12:07:10 -0300

    revisao

---
+++ revisao/lista2/exercicio1.py
@@ -0,0 +1,12 @@
+
+# esse solicita o nome do usuário  e nome do pai e da mãe
+nome_usuario = input("Digite o seu nome: ")
+nome_pai = input("Digite o nome do seu pai: ")
+nome_mae = input("Digite o nome da sua mãe: ")
+
+print("Nome do Usuário:")
+print(nome_usuario)
+
+print("Nome dos Pais:")
+print(f"{nome_pai} e {nome_mae}")
+
---
+++ revisao/lista2/exercicio2.py
@@ -0,0 +1,8 @@
+def calculaSoma(num1, num2):
+    resultado = num1 + num2
+    print("A soma dos dois números é:", resultado)
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+calculaSoma(num1, num2)
